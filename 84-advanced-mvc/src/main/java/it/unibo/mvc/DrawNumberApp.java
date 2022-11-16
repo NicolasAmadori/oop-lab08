de9e24b73d49a5db29b3cec111eb00aaf7349a80
Nicolas Amadori <nicolasamadori2002@gmail.com>
2022-11-16 23:42:43 +0100

    Finito esercizio 84

--- 84-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
+++ 84-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
@@ -1,15 +1,30 @@
 package it.unibo.mvc;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+    private static final String SEPARATOR = System.getProperty("file.separator");
+    private static final String PROJECT_DIR = System.getProperty("user.dir");
+    private static final String CFG_FILE_RELATIVE_PATH = "src" + SEPARATOR + "main" + SEPARATOR + "resources";
+    private static final String CFG_FILE_NAME = "config.yml";
+    private static final String CFG_FILE_ABSOLUTE_PATH =
+        PROJECT_DIR + SEPARATOR + CFG_FILE_RELATIVE_PATH + SEPARATOR + CFG_FILE_NAME;
+
+    private static final String OUTPUT_FILE_PATH = System.getProperty("user.home");
+    private static final String OUTPUT_FILE_NAME = "output.txt";
+
+    private int min = 0;
+    private int max = 100;
+    private int attempts = 10;
 
     private final DrawNumber model;
     private final List<DrawNumberView> views;
@@ -27,7 +42,18 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
             view.setObserver(this);
             view.start();
         }
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+        this.readConfigurationSettings();
+        this.model = new DrawNumberImpl(min, max, attempts);
+    }
+
+    private void readConfigurationSettings() {
+        try (BufferedReader br = new BufferedReader(new FileReader(CFG_FILE_ABSOLUTE_PATH))) {
+            min = Integer.parseInt(br.readLine().split(":")[1].trim());
+            max = Integer.parseInt(br.readLine().split(":")[1].trim());
+            attempts = Integer.parseInt(br.readLine().split(":")[1].trim());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     @Override
@@ -66,7 +92,13 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
      * @throws FileNotFoundException 
      */
     public static void main(final String... args) throws FileNotFoundException {
-        new DrawNumberApp(new DrawNumberViewImpl());
+        new DrawNumberApp(new DrawNumberViewImpl(),
+                          new DrawNumberViewImpl(),
+                          new PrintStreamView(
+                            new PrintStream(
+                                new FileOutputStream(OUTPUT_FILE_PATH + SEPARATOR + OUTPUT_FILE_NAME))),
+                          new PrintStreamView(System.out),
+                          new PrintStreamView(System.out));
     }
 
 }
